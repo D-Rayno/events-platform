4d7307addea5fb4621a286e7768d7b264b153077
Rezougui Rayan Zin Eddine <r.rezougui@aymenpromotion.com>
2025-10-14 10:17:48 +0100

    fix: routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,8 +1,5 @@
-import router from '@adonisjs/core/services/router'
 import apiRoutes from './routes/api.js'
 import webRoutes from './routes/web.js'
 
-router.group(() => {
-  apiRoutes()
-  webRoutes()
-})
+apiRoutes()
+webRoutes()
